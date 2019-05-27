cdc464db352fee0a0a1d36cc6fc04e28f448a9d3
yuwen <houyuwen2014@163.com>
2019-05-27 11:02:48 +0800

    最小连续数组

--- 【001】数组和字符串/src/summary/rotate.java
+++ 【001】数组和字符串/src/summary/rotate.java
@@ -49,8 +49,23 @@ public class rotate {
         System.arraycopy(s1, 0, res, s2.length, s1.length);
         nums = res;
         System.out.println(Arrays.toString(nums));
-
-
-
     }
+
+    public void rotate_soultion(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums [start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+
 }
